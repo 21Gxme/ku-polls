df7b582d53cc2d2e8f33e93aaa6fb0994ac6bebb
Tantikorn Prasanpeangsi <tantikorn@Macintosh-5.local>
2023-09-06 15:53:26 +0700

    create can_vote and is_publish

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,9 @@
-from typing import Any
-from django.db import models
-from django.db.models.query import QuerySet
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.contrib import messages
 
 from .models import Question, Choice
 
@@ -32,16 +30,32 @@ class DetailView(generic.DetailView):
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
+    def get(self, request, *args, **kwargs):
+        try:
+            question = get_object_or_404(Question, pk=kwargs["pk"])
+        except Http404:
+            messages.error(request, f"Poll number {kwargs['pk']} does not exist.")
+            return redirect("polls:index")
+
+        if question.can_vote():
+            return render(request, self.template_name, {"question": question})
+        else:
+            messages.error(request, f"Poll number {question.id} is not available to vote")
+            return redirect("polls:index")
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+    def get(self, request, *args, **kwargs):
+        try:
+            question = get_object_or_404(Question, pk=kwargs["pk"])
+        except Http404:
+            messages.error(request,
+                           f"Poll number {kwargs['pk']} does not exist.")
+            return redirect("polls:index")
+        return render(request, self.template_name, {"question": question})
 
 
 def vote(request, question_id):
@@ -61,3 +75,5 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
