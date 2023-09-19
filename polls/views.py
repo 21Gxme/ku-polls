bfdb592326335b5597bb6c1e70c050dd12304668
Tantikorn Prasanpeangsi <tantikorn@Macintosh-5.local>
2023-09-19 15:08:39 +0700

    displays all published polls

--- polls/views.py
+++ polls/views.py
@@ -17,7 +17,7 @@ class IndexView(generic.ListView):
         to be published in the future).
         """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
-            '-pub_date')[:5]
+            '-pub_date')
 
 
 class DetailView(generic.DetailView):
